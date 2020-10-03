60fc6f492941fc1f47ce3fd11e6e65f1509bc5cc
Utku Gultopu <ugultopu@gmail.com>
2020-10-03 14:48:33 -0400

    Create the form component in React
    In order to make a request to GitHub GraphQL API and get results.

--- src/App.js
+++ src/App.js
@@ -13,10 +13,45 @@ const axiosGitHubGraphQL = axios.create({
 const TITLE = 'React GraphQL GitHub Client';
 
 class App extends Component {
+  state = {
+    path: 'the-road-to-learn-react/the-road-to-learn-react',
+  };
+
+  componentDidMount() {
+    // fetch data
+  }
+
+  onChange = event => {
+    this.setState({ path: event.target.value });
+  }
+
+  onSubmit = event => {
+    // fetch data
+    event.preventDefault();
+  }
+
   render() {
+    const { path } = this.state;
+
     return (
       <div>
         <h1>{TITLE}</h1>
+
+        <form onSubmit={this.onSubmit}>
+          <label htmlFor="url">
+            Show open issues for https://github.com/
+          </label>
+          <input
+            id="url"
+            type="text"
+            value={path}
+            onChange={this.onChange}
+            style={{ width: '300px' }}
+          />
+          <button type="submit">Search</button>
+        </form>
+        <hr />
+        {/* Here comes the result! */}
       </div>
     );
   }
