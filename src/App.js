4990cc5db47f4d1a1bda65a8d5bda54eb74b1db5
Utku Gultopu <ugultopu@gmail.com>
2020-10-03 21:12:51 -0400

    Display the errors if one or more errors occur

--- src/App.js
+++ src/App.js
@@ -88,13 +88,23 @@ class App extends Component {
   }
 }
 
-const Organization = ({ organization }) => (
-  <div>
-    <p>
-      <strong>Issues from Organization:</strong>
-      <a href={organization.url}>{organization.name}</a>
-    </p>
-  </div>
-);
+const Organization = ({ organization, errors }) => {
+  if (errors) {
+    return (
+      <p>
+        <strong>Something went wrong:</strong>
+        {errors.map(error => error.message).join(' ')}
+      </p>
+    );
+  }
+  return (
+    <div>
+      <p>
+        <strong>Issues from Organization:</strong>
+        <a href={organization.url}>{organization.name}</a>
+      </p>
+    </div>
+  );
+};
 
 export default App;
